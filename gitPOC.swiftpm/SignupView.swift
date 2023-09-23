bd8abf666712204ccaba9911ce1a3f2403fee67a
Iosdhilipan <dhilipan.r@inovark.com>
2023-09-23 17:23:22 +0530

    fist message

---
+++ gitPOC.swiftpm/SignupView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  gitPOC
+//
+//  Created by Apple on 23/09/23.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text("SignupView")
+    }
+}
+
+struct SignupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupView()
+    }
+}
